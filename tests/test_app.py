bbceb1a2ef3852fafabc8c936a8c37073a78b9e6
Kayleigh Rix <rixka@outlook.com>
2018-03-16 22:27:24 +0000

    [fix] error handling for 404s
    Previous 404 error returned HTML not json so I implemented a catch all route to respond with 404.

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,19 @@ from flask import url_for
 from app import utils
 
 
-def test_health(accept_json, client):
-    res = client.get(url_for('api.health'), headers=accept_json)
+class TestApp(object):
 
-    assert res.status_code == 200
-    assert res.mimetype == utils.JSON_MIME_TYPE
-    assert res.json == {'status': 'ok'}
+    def test_health(self, accept_json, client):
+        res = client.get(url_for('api.health'), headers=accept_json)
+    
+        assert res.status_code == 200
+        assert res.mimetype == utils.JSON_MIME_TYPE
+        assert res.json == {'status': 'ok'}
+
+    def test_not_found(self, accept_json, client):
+        res = client.get('/', headers=accept_json)
+
+        assert res.status_code == 404
+        assert res.mimetype == utils.JSON_MIME_TYPE
+        assert res.json == {'error': 'Not Found'}
 
