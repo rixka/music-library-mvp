1c59d6562af0aa7f59c2d0deebc381f6da1f54e2
Kayleigh <rixka@users.noreply.github.com>
2018-03-16 22:29:49 +0000

    [fix] error handling for 404s (#2)
    Previous 404 error returned HTML not json so I implemented a catch all route to respond with 404.

--- app/api.py
+++ app/api.py
@@ -1,10 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, abort
 from utils import json_response
 
 api = Blueprint('api', __name__)
 
 
+@api.route('/', defaults={'path': ''})
+@api.route('/<path:path>')
+def catch_all(path):
+    abort(404)
+
 @api.route("/health")
 def health():
     return json_response({'status': 'ok'}, 200)
+
+
+# === HANDLERS === #
+
+@api.errorhandler(404)
+def not_found(error):
+    return json_response({'error': 'Not Found'}, 404)
 
